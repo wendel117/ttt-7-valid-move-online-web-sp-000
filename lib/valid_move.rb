f820b3eea97fd72316fb81bac92a18bf10c9dcc4
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-23 22:56:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,9 @@
 # code your #valid_move? method here
-
+def valid_move?
+  if input.between?(0, 8)
+    return true
+  else
+    return false
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
